ee4d2f0bc0cbfe89b1bed67b249483902846f6f8
Vojin Puric <puricvojin@gmail.com>
2020-01-01 17:26:38 +0100

    notes fully working

--- scriptNotes.js
+++ scriptNotes.js
@@ -3,9 +3,13 @@ const activeDisplay = document.querySelector('[data-list-display-container]');
 const newNoteInput = document.querySelector('[data-new-note-input]');
 
 const LOCAL_STORAGE_NOTE_KEY = 'note.lists';
-const LOCAL_STORAGE_SELECTED_NOTE_ID = 'note.selected';
+const LOCAL_STORAGE_NOTE_KEY = 'lists.lists';
+const LOCAL_STORAGE_SELECTED_ID = 'object.selected.id';
+
 let notes = JSON.parse(localStorage.getItem(LOCAL_STORAGE_NOTE_KEY)) || [];
-let selectedNoteId = localStorage.getItem(LOCAL_STORAGE_SELECTED_NOTE_ID);
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_NOTE_KEY)) || [];
+
+let selectedId = localStorage.getItem(LOCAL_STORAGE_SELECTED_ID);
 let active = null;
 let activeIndex =0;
 
@@ -14,7 +18,7 @@ let activeIndex =0;
 notesContainer.addEventListener('click', e => {
     if (e.target.tagName.toLowerCase() == 'li') {
         activeDisplay.classList.remove("hidden");
-        selectedNoteId = e.target.dataset.noteId;
+        selectedId = e.target.dataset.noteId;
         console.log(e.target.dataset.noteId)
         save();
         render();
@@ -26,6 +30,7 @@ document.querySelector('[data-new-note-form]').addEventListener('submit', e => {
     const noteName = newNoteInput.value
     if (noteName == null || noteName === '') return
     const note = createNote(noteName)
+    selectedId= note.id;
     newNoteInput.value = null
     notes.push(note)
     save();
@@ -42,26 +47,27 @@ function clearElement(element) {
 function createNote(name) {
     return { id: Date.now().toString(), name: name, text: "" }
 }
+
 //delete button
 document.querySelector('[data-delete-button]').addEventListener('click', e => {
-    notes = notes.filter(list => list.id !== selectedNoteId);
-    selectedNoteId = null;
+    notes = notes.filter(list => list.id !== selectedId);
+    selectedId = null;
     save();
     render();
 });
 //saving data
 function save() {
     localStorage.setItem(LOCAL_STORAGE_NOTE_KEY, JSON.stringify(notes));
-    localStorage.setItem(LOCAL_STORAGE_SELECTED_NOTE_ID, selectedNoteId);
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_ID, selectedId);
 }
 //displaying data
 function render() {
     clearActive();
-    if (selectedNoteId == "null" || selectedNoteId === null) {
+    if (selectedId == "null" || selectedId === null) {
         activeDisplay.classList.add("hidden");
     } else {
         for(let i=0;i<notes.length;i++){
-            if (notes[i].id == selectedNoteId) {
+            if (notes[i].id == selectedId) {
                 active = notes[i];
                 activeIndex=i;
                 break
@@ -72,8 +78,8 @@ function render() {
         const textArea =document.createElement("p");
         textArea.contentEditable=true;
         textArea.addEventListener('keypress',()=>{
-            console.log(i)
-            notes[i].text=textArea.innerText;
+            notes[activeIndex].text=textArea.innerText;
+            save();
         })
         textArea.innerHTML=active.text;
         activeDisplay.querySelector(".todo-body").append(textArea);
@@ -85,7 +91,7 @@ function render() {
         listElement.dataset.noteId = note.id;
         listElement.classList.add("list-name");
         listElement.innerHTML = note.name;
-        if (note.id === selectedNoteId) {
+        if (note.id === selectedId) {
             listElement.classList.add("active-list");
         }
         notesContainer.appendChild(listElement);
